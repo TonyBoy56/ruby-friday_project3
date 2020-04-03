6b6404d20f191735181a524097e4dbec2b42c2c4
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-03 11:13:32 -0700

    add function to find range of user's input. Push counter into empty array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 //////////////////////////////// Back-end logic ////////////////////////////////
 
-
-
-
+function findTheRange(theirInput) {
+  var rangeArray = [];
+  for (var i= 0; i <= theirInput; i++) {
+    rangeArray.push(i);
+  }
+}
 //////////////////////////////// Front-end logic ////////////////////////////////
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    var theirInput = $("#input").val();
+    var theirInput = parseInt($("#userInput").val());
+    console.log(theirInput);
+    $("#output").text(theirInput);
   });
 });
